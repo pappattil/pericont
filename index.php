bcbc82c5716171150db15f87f0503ae84dc98a1c
Papp Attila <35372822+pappattil@users.noreply.github.com>
2018-10-31 15:54:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+ 
+<meta name="description" content="Pericont">
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link href="https://fonts.googleapis.com/css?family=Space+Mono:400,700" rel="stylesheet">
+  <link href="index.css" rel="stylesheet">
+    
+  <title>Pericont-Bau Kft</title>
+</head>
+<body>
+  
+  
+  
+  
+  
+  <div style="height: 7vh;">
+    </div>
+ 
+  
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark ">
+  <a class="navbar-brand" href="tel:+36203385608">Pericont<p>Telefonszám:+36(12)345-6789</p></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav ">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kezdőlap <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Szolgáltatások </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Referenciák </a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kapcsolat </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Partnerek </a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+  
+  
+  
+
+  <header>
+    
+    <div class="wrapper">
+       <p class="headline" >&bdquo;Elképzeléstől a megvalósulásig&rdquo;</p>
+        <br/><br/>
+    <div class="locations">
+    <a href="https://webaty.hu"><div class="l line">
+      <h3>Generálkivitelezés</h3>
+      <img src="https://admin.webgenerator.hu/uploads_customer/11112456/stockfresh_4999216_layout_cottag.jpg">
+      <ul>
+        <li>nec non erat</li>
+        <li>massa eu viverra </li>
+        <li>sollicitudin consequat </li>
+      </ul>
+    </div>
+      </a>
+    <a href="https://webaty.hu"><div class="l line">
+      <h3>Belső munkák</h3>
+      <img src="http://alomhazprojekt.hu/wp-content/uploads/2017/04/general1.jpg">
+      <ul>
+        <li>nec non erat</li>
+        <li>massa eu viverra </li>
+        <li>sollicitudin consequat </li>
+      </ul>
+    </div></a>
+    
+    <a href="https://webaty.hu"><div class="l line">
+      <h3>Egyéb</h3>
+      <img src="http://epitesvallalkozas.hu/ujoldal/wp-content/uploads/2015/11/kiv01_mas.png">
+      <ul>
+        <li>nec non erat</li>
+        <li>massa eu viverra </li>
+        <li>sollicitudin consequat </li>
+      </ul>
+    </div>
+            </a>
+  </div>
+        <br/>
+
+      </div>
+ 
+  </header>
+  <content>
+  <h2>  Vestibulum vitae libero iaculis</h2>
+  <p>feugiat sapien cursus, condimentum arcu. Donec volutpat, nisi condimentum tincidunt elementum, ante magna tempor metus, quis molestie augue mauris vel metus. Aenean nisl risus, vehicula vitae venenatis vel, blandit feugiat libero. Morbi ultricies lacus vehicula urna lobortis iaculis nec non erat. Pellentesque id magna dui. Ut tellus dui, dictum sit amet efficitur et, iaculis nec justo. Vivamus placerat, massa eu viverra lacinia, nulla nibh efficitur augue, in tempus lorem arcu et ante. Proin facilisis hendrerit faucibus. Aliquam dapibus ligula in sollicitudin maximus. Mauris eleifend odio ut tincidunt sollicitudin. Fusce vel pulvinar metus. Etiam pulvinar sollicitudin consequat. Etiam ac consequat metus. Nullam molestie pulvinar elit et accumsan
+ </p>   
+ </content>
+  <div class="foot">
+    <div class="foot-w">
+      <div class="row">
+        <div class="col-md-3 col-md-offset-1 ">
+      <p><img src="https://yoursbody.com/pericont/logo2.jpg">
+        <p>Kulcsár Péter          </p>
+          <small>Ügyvezető </small>
+          <p>e-mail: <a href="mailto:kulcsarpeter@gmail.com">kulcsarpeter@gmail.com</a></p>
+          <p>Iroda: Ráckeve, Árpád tér 4.</p>
+          
+          </div>
+        <div class="col-md-3">
+      <p></p>
+          </div>
+        <div class="col-md-3">
+      <p></p>
+          </div>
+      </div>
+      
+      <p><small><a href="#">Adatvédelmi irányelvek</a> | PERICONT-BAU &#64; 2018 | <a href="https://webaty.hu">webmester</a> </small></p>
+    </div>
+  </div>
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
